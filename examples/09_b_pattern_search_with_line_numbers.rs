ea8706b6cf3005a05648fc4749c76912a9e2481f
remiconnesson <remiconnesson2@gmail.com>
2022-08-02 09:50:53 +0000

    Count lines with enumerate()

--- examples/09_b_pattern_search_with_line_numbers.rs
+++ examples/09_b_pattern_search_with_line_numbers.rs
@@ -6,12 +6,10 @@ dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
 
-    let mut line_num: usize = 1;
-
-    for line in quote.lines() {
+    for (i, line) in quote.lines().enumerate() {
+        let line_num = i + 1;
         if line.contains(search_term) {
             println!("{}: {}", line_num, line);
         }
-        line_num += 1;
     }
 }
