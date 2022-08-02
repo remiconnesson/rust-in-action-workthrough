17476595f0e1696b6d62a49d0917169071123a58
remiconnesson <remiconnesson2@gmail.com>
2022-08-02 14:17:23 +0000

    Small optimization to spare an ops per cycle

--- examples/09_b_pattern_search_with_line_numbers.rs
+++ examples/09_b_pattern_search_with_line_numbers.rs
@@ -7,8 +7,8 @@ It is the same with books.
 What do we seek through millions of pages?";
 
     for (i, line) in quote.lines().enumerate() {
-        let line_num = i + 1;
         if line.contains(search_term) {
+            let line_num = i + 1; // in the loop to avoid unnecessary ops
             println!("{}: {}", line_num, line);
         }
     }
